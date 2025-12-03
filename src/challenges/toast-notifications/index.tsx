50dc49e828ac019e7497d1246b0264c8faf72509
Ross Noble <rnoble@fastmail.com>
2025-12-03 10:37:06 -0500

    Add hook to expose toast access

--- src/challenges/toast-notifications/index.tsx
+++ src/challenges/toast-notifications/index.tsx
@@ -1,19 +1,27 @@
-// type Variant = "info" | "success" | "error" | "warning"
+type Variant = 'info' | 'success' | 'error' | 'warning'
 
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 type Position = 'top-left' | 'top-right'
 
 type ToastProps = {
   message: string
-  isOpen?: boolean
   position?: Position
+  variant?: Variant
+  isOpen: boolean
+  onClose: () => void
 }
 
 export function Toast(props: ToastProps) {
-  const { message, isOpen = false, position = 'top-right' } = props
+  const {
+    isOpen = false,
+    message,
+    position = 'top-right',
+    variant = 'info',
+    onClose,
+  } = props
 
-  if (!isOpen) return null
+  const timeoutRef = useRef(0)
 
   const getPositionStyles = (position: Position) => {
     const styles: Record<Position, string> = {
@@ -24,24 +32,89 @@ export function Toast(props: ToastProps) {
     return styles[position] ?? ''
   }
 
+  const getVariantStyles = (variant: Variant) => {
+    const styles: Record<Variant, string> = {
+      info: 'border-blue-400 bg-blue-100 ',
+      success: 'border-green-400 bg-green-100',
+      error: 'border-red-400 bg-red-100',
+      warning: 'border-orange-400 bg-orange-100',
+    }
+
+    return styles[variant] ?? ''
+  }
+
+  useEffect(() => {
+    if (!isOpen) return
+
+    timeoutRef.current = setTimeout(() => {
+      onClose?.()
+    }, 2000)
+
+    return () => {
+      timeoutRef.current = 0
+    }
+  }, [isOpen])
+
+  if (!isOpen) return null
+
   return (
     <div
-      className={`${getPositionStyles(position)} fixed rounded-md border border-blue-400 bg-blue-100 px-6 py-4 shadow-md`}
+      className={`${getPositionStyles(position)} ${getVariantStyles(variant)} fixed rounded-md border px-6 py-4 shadow-md`}
     >
       {message}
     </div>
   )
 }
 
-export function ToastContainer() {
+const useToast = () => {
   const [showToast, setShowToast] = useState(false)
+  const [variant, setVariant] = useState<Variant>('info')
+  const [message, setMessage] = useState('')
+
+  const onClose = () => setShowToast(false)
+
+  return {
+    toast: (
+      <Toast
+        message={message}
+        variant={variant}
+        isOpen={showToast}
+        onClose={() => setShowToast(false)}
+      />
+    ),
+    showToast: ({
+      message,
+      variant,
+    }: Partial<Pick<ToastProps, 'message' | 'variant'>>) => {
+      if (message) setMessage(message)
+      if (variant) setVariant(variant)
+      setShowToast(true)
+    },
+    onClose,
+  }
+}
+
+export function ToastContainer() {
+  const { toast, showToast } = useToast()
 
   return (
     <div>
-      <button onClick={() => setShowToast(prev => !prev)} className="button">
-        Trigger toast
+      <button
+        onClick={() => showToast({ message: 'This is a message 1' })}
+        className="button"
+      >
+        Trigger toast 1
       </button>
-      <Toast message="This is a notification message" isOpen={showToast} />
+      <button
+        onClick={() =>
+          showToast({ variant: 'warning', message: 'This is a message 2' })
+        }
+        className="button"
+      >
+        Trigger toast 2
+      </button>
+
+      {toast}
     </div>
   )
 }
